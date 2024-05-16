2775b2979287ae506417162ff6604531c4fa93e7
swgc-shota <137166117+swgc-shota@users.noreply.github.com>
2024-05-16 13:29:24 +0900

    Synchronize directory structure with main-ja branch

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { defineConfig, loadEnv } from "vite";
+export default defineConfig(({ command }) => {
+  const env = loadEnv("", process.cwd(), "");
+  const config = { root: "src" };
+
+  if (command === "build") {
+    if (env.VITE_BASE_PATH !== undefined) {
+      config.base = env.VITE_BASE_PATH;
+    }
+  }
+
+  if (env.VITE_KEY_PATH !== undefined && env.VITE_CERT_PATH !== undefined) {
+    config.server = {
+      https: {
+        key: fs.readFileSync(env.VITE_KEY_PATH),
+        cert: fs.readFileSync(env.VITE_CERT_PATH),
+      },
+    };
+  }
+
+  return config;
+});
