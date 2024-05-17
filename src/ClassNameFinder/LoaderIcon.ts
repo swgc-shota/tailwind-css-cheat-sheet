670a6f426f7da3e0ccb69267771e9c7d391054e2
swgc-shota <137166117+swgc-shota@users.noreply.github.com>
2024-05-17 11:40:55 +0900

    Adjust the position of the loader icon

--- src/ClassNameFinder/LoaderIcon.ts
+++ src/ClassNameFinder/LoaderIcon.ts
@@ -5,7 +5,7 @@ export const LoaderIcon = (isShow: State<boolean>) =>
   div(
     {
       class: () =>
-        `absolute left-[16px] top-[20%]  text-yellow-500 ${
+        `absolute left-[16px] top-[20%] text-yellow-500 ${
           isShow.val ? "loader left-[18px] top-[6px] text-xl" : "text-md"
         }`,
     },
